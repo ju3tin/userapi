f58cae9106e444dfa6f39845540ffa67749f8029
ju3tin <ju3tin@hotmail.co.uk>
2024-09-12 12:40:04 +0100

    update

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
 
 // API route to check if an email exists in the database
 app.get('/api/check-email', async (req, res) => {
-  const email = req.query.email;
+  const email = req.body.email;
 
   if (!email) {
     return res.status(400).json({ success: false, message: 'Email query parameter is required' });
